d6d4e9c358660a8d79676664bf6e615bbd962f27
ophois34 <ophois34@gmail.com>
2021-06-24 09:28:24 +0200

    mock.db + db.json

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
+import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
+import { PageListOrderComponent } from './pages/page-list-order/page-list-order.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageAddOrderComponent,
+    PageEditOrderComponent,
+    PageListOrderComponent
+  ],
   imports: [
     CommonModule,
     OrdersRoutingModule
