cf3289765dfa7ced03d4b885032c2a19d1311879
ophois34 <ophois34@gmail.com>
2021-06-24 09:19:59 +0200

    affichage service version pipe async

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { VersionService } from '../../services/version.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  /* version$: le $ parce que c'est un observable.
+               le ! parce que sans valeur au moment de la définition */
+  public version$!: BehaviorSubject<number>;
+
+  constructor(private vs: VersionService) {
+    this.version$ = this.vs.version$;
+  }
 
   ngOnInit(): void {
   }
